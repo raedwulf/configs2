a227b3da728ae2324b7731306828a89fa16f7027
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2013-08-12 11:22:10 +0100

    sxhkd: update to use tmux and st

--- link.sh
+++ link.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+mkdir -p $HOME/.config
+mkdir -p $HOME/bin
+
 for i in `echo "meat bspwm sxhkd"`; do
-	mkdir -p $HOME/.config
 	ln -s $HOME/config/dotconfig/$i $HOME/.config/
 done
+
+ln -s $HOME/config/.tmux.conf $HOME/.tmux.conf
