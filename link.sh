4b076a8c04f9ddd86df2fd91b704d4fc79fd936a
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2013-08-12 11:27:42 +0100

    link: add tmx to bin folder

--- link.sh
+++ link.sh
@@ -7,4 +7,5 @@ for i in `echo "meat bspwm sxhkd"`; do
 	ln -s $HOME/config/dotconfig/$i $HOME/.config/
 done
 
+ln -s $HOME/config/bin/tmx $HOME/bin/tmx
 ln -s $HOME/config/.tmux.conf $HOME/.tmux.conf
