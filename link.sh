ad7baf390d8d014190118f0205da256c6f7a6d62
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2013-09-07 11:25:52 +0100

    link: add vim config linking

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir -p $HOME/{.config,bin,downloads}
+mkdir -p $HOME/{.config,bin,downloads,.vim}
 
 for i in `echo "meat bspwm sxhkd jumanji compton"`; do
 	ln -sf $HOME/config/config/$i $HOME/.config/
@@ -10,6 +10,10 @@ for i in `echo "tmx"`; do
 	ln -sf $HOME/config/bin/$i $HOME/bin/$i
 done
 
-for i in `echo "tmux.conf bashrc xinitrc inputrc"`; do
+for i in `echo "tmux.conf bashrc xinitrc inputrc vimrc"`; do
 	ln -sf $HOME/config/$i $HOME/.$i
+done
+
+for i in `echo "autoload colors doc plugin syntax"`; do
+	ln -sf $HOME/config/vim/$i $HOME/.vim/
 done
