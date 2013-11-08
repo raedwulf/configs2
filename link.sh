0428f687030e8b1fe0da78cfc560d2565ea27ab9
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2013-11-08 21:10:08 +0000

    bspwm: updated to latest head

--- link.sh
+++ link.sh
@@ -6,7 +6,7 @@ for i in `echo "meat bspwm sxhkd jumanji compton"`; do
 	ln -sf $HOME/config/config/$i $HOME/.config/
 done
 
-for i in `echo "tmx"`; do
+for i in `echo "tmx ruld rulc rule_command"`; do
 	ln -sf $HOME/config/bin/$i $HOME/bin/$i
 done
 
